04d8f40c5e1b72e010feac2735e29bfe635a11e0
LvChengbin <lvchengbin59@gmail.com>
2020-08-07 19:21:55 +0800

    u

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/******************************************************************
+ * Copyright (C) 2020 LvChengbin
+ * 
+ * File: src/index.ts
+ * Author: LvChengbin<lvchengbin59@gmail.com>
+ * Time: 08/07/2020
+ * Description: 
+ ******************************************************************/
+
---
+++ src/table.ts
@@ -0,0 +1,26 @@
+/******************************************************************
+ * Copyright (C) 2020 LvChengbin
+ * 
+ * File: src/table.ts
+ * Author: LvChengbin<lvchengbin59@gmail.com>
+ * Time: 08/07/2020
+ * Description: 
+ ******************************************************************/
+
+const offcut = {
+    t: '─', // top
+    tm : '┬', // top middle
+    tl : '┌', // top left
+    tr : '┐', // top right
+    b : '─', // bottom
+    bm : '┴', // bottom middle
+    bl : '└', // bottom left
+    br : '┘', // bottom right
+    l : '│', // left
+    ml : '├', // right
+    mh : '─', // middle horizontal
+    mm : '┼', // middle middle
+    r : '│', // right
+    mr : '┤', // middle right
+    mv : '│' // middle vertical 
+}
